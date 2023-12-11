ed36eb20a3bc4a8201f1f10d91cd0e85238f1f37
MichaelKazarian <28EMACSlisp>
2023-12-11 12:40:46 +0200

    Read encoder BCD8421 output

--- ENC-EP50S8/src/main.cpp
+++ ENC-EP50S8/src/main.cpp
@@ -1,37 +1,73 @@
 #include <Arduino.h>
+#define BITS 11
+#define LSB 12
+
+void gpioInit();
+void readRaw(int *a);
+void printRaw(int *a);
+int bcd8421(int *a, int length, int factor);
+int rawToBin(int *a);
 
 void setup() {
   Serial.begin(9600);
-  #if ((F_CPU) == 1000000)
-  #warning F_CPU = 1MHz
-  Serial.println("1MHz");
-  #elif ((F_CPU) == 2000000)
-  #warning F_CPU = 2MHz
-  Serial.println("2MHz");
-  #elif ((F_CPU) == 4000000)
-  #warning F_CPU = 4MHz
-  Serial.println("4MHz");
-  #elif ((F_CPU) == 8000000)
-  #warning F_CPU = 8MHz
-  Serial.println("8MHz");
-  #elif ((F_CPU) == 16000000)
-  #warning F_CPU = 16MHz
-  Serial.println("16MHz");
-  #elif ((F_CPU) == 32000000)
-  #warning F_CPU = 32MHz
-  Serial.println("32MHz");
-  #endif
-
-  #if ((CLOCK_SOURCE) == 1)
-  Serial.println("internal clock");
-  #elif ((CLOCK_SOURCE) == 2)
-  Serial.println("external clock");
-  #endif
-
-  //while(1);
+  gpioInit();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int rawValues[BITS];
+  readRaw(rawValues);
+  printRaw(rawValues);
+  Serial.print("\t");
+  Serial.println( rawToBin(rawValues) );
+  Serial.print("\n");
+  delay(100);
+}
+
+void gpioInit() {
+  int i;
+  for (i = LSB; i > LSB - BITS; i--){
+    pinMode(i, INPUT_PULLUP);
+  }
+}
+
+int bcd8421(int *a, int length, int factor){
+  const int weight[] = {1, 2, 4, 8};
+  int sum = 0;
+  for (int i=0; i<length; ++i)
+    sum += a[i]*weight[i];
+  return sum*factor;
+};
+
+void readRaw(int *a) {
+  int i, j=BITS-1;
+  for (i = LSB; i > LSB-BITS; i--){ 
+    int readVal;
+    readVal = !digitalRead(i);
+    a[j] = readVal;
+    j--;
+  }
+}
+
+void printRaw(int *a) {
+  for(int j = BITS-1; j >=0; j--){
+    if (j==7 || j == 3) {
+      Serial.print(" ");
+    };
+    Serial.print(a[j]);
+  }
+}
+
+int rawToBin(int *a) {
+  int x1   = bcd8421(a, 4 ,1);
+  int x10  = bcd8421(&a[4], 4 ,10);
+  int x100 = bcd8421(&a[8], 3 ,100);
+  return x1+x10+x100;
+}
+
+void greytobinary(int *a, int count) {
+  int i;
+  for(i = count - 2; i >= 0; i--) {
+    a[i] = a[i]^a[i+1];
+  }
 }
 
