784d4a18a4b301ff7dd80651513261086a290b47
MichaelKazarian <28EMACSlisp>
2023-12-28 14:39:54 +0200

    Improved completion

--- ENC-EP50S8/src/.dir-locals.el
+++ ENC-EP50S8/src/.dir-locals.el
@@ -7,10 +7,21 @@
 ;; Requirements: 
 ;; Status: not intended to be distributed yet
 
-((nil . ((company-clang-arguments . (
-                                     "-I/home/kazarian/.platformio/packages/framework-lgt8fx/libraries/Wire"))
-         (eval . (progn
-                   (add-to-list 'company-backends '(company-etags company-clang))))
+((nil . ((eval . (progn
+                   (setq-local tags-candidate '("."))
+                   (setq-local clang-tags-candidate
+                               '("~/.platformio/packages/framework-lgt8fx/cores"
+                                 "~/.platformio/packages/framework-lgt8fx/libraries"
+                                 "~/MY_Projects/LAC"
+                                 "."))
+                   (setq-local company-clang-arguments (clang-src-dirs))
+                   (add-to-list 'company-backends '(company-etags company-clang))
+                   (add-hook 'after-save-hook
+                             (lambda ()
+                               (etags-tag-create (find-dirs-str))))
+                   ))
+         (eval . (git-gutter-mode))
+         (eval . (yas-minor-mode-on))
          (eval . (message ".dir-locals.el was loaded"))
          )))
 
