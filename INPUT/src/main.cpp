d0836b0dcd225f19036a886d73f628e27fd6cdcb
MichaelKazarian <28EMACSlisp>
2024-03-18 11:16:55 +0200

    Reading inuts

--- INPUT/src/main.cpp
+++ INPUT/src/main.cpp
@@ -1,35 +1,79 @@
 #include <Arduino.h>
 #include <SimpleModbusSlave.h>
+#include <MCP23017.h>
 
-#define PRINT_SPEED 5 // Modbus update per second
+#define PRINT_SPEED 100 // Modbus update per second
 #define BAUD_RATE 9600
 #define BITS 64
+#define MCP_LAST_PIN 6 // MCP23017 has problematic GPA7/GPB7
+#define BOARD_IN_FIRST 7
+#define BOARD_IN_LAST 10
+
+MCP23017 mcp0 = MCP23017(0x20);
+MCP23017 mcp1 = MCP23017(0x21);
+
 void gpioInit();
+void mcpInit();
+void registersInit();
+void readPins();
 
 
 const int MODBUS_ID = 10;
-const int HOLDING_REGS_SIZE = 1;
-unsigned int modbusHoldingRegisters[HOLDING_REGS_SIZE] = {0};
+const int HOLDING_REGS_SIZE = 32;
+unsigned int holdingRegisters[HOLDING_REGS_SIZE] = {};
 
 int RAWVALUES [BITS] = {};
 
 void setup() {
-  Serial.begin(BAUD_RATE);
-  // modbus_configure(BAUD_RATE, MODBUS_ID, 1, HOLDING_REGS_SIZE, 0);
+  // Serial.begin(BAUD_RATE);
+  registersInit();
   gpioInit();
+  mcpInit();
+  modbus_configure(BAUD_RATE, MODBUS_ID, 1, HOLDING_REGS_SIZE, 0);
 }
 
 void loop() {
-  // modbusHoldingRegisters[0] = degree;
-  // printRaw(RAWVALUES); Serial.print("\t");
-  // Serial.println( modbusHoldingRegisters[0] );
-  // modbus_update(modbusHoldingRegisters);
+  modbus_update(holdingRegisters);
+  readPins();
   delay(PRINT_SPEED);
 }
 
-void gpioInit() {
-  // for (int i = 0; i < BITS; i++) {
-  //   pinMode(PINS[i], INPUT_PULLUP);
-  // }
+void registersInit() {
+  for (int i=0; i< HOLDING_REGS_SIZE; i++) holdingRegisters[i] = 0;
 }
 
+void gpioInit() {
+  for (int i=BOARD_IN_FIRST; i<= BOARD_IN_LAST; i++) pinMode(i, INPUT_PULLUP);
+}
+
+/**
+ * \brief Inits MCP23017 devices.
+ */
+void mcpInit() {
+  // Init MCP23017 adressing pins
+  pinMode(2, OUTPUT);  digitalWrite(2, LOW);   // MCP0, addr 20
+  pinMode(A1, OUTPUT); digitalWrite(A1, LOW);  // MCP1, addr 21
+  pinMode(A2, OUTPUT); digitalWrite(A2, LOW);
+  pinMode(A3, OUTPUT); digitalWrite(A3, HIGH);
+  mcp0.init();
+  mcp1.init();
+  for (int i=0; i<= MCP_LAST_PIN; i++) { // GPA/GPB init
+     mcp0.pinMode(i, INPUT_PULLUP); mcp0.pinMode(i+8, INPUT_PULLUP);
+     mcp1.pinMode(i, INPUT_PULLUP); mcp1.pinMode(i+8, INPUT_PULLUP);
+  }
+}
+
+void readPins() {
+  for (int i=0; i<= MCP_LAST_PIN; i++) {
+    holdingRegisters[i] = mcp0.digitalRead(i);       // Reading MCP0 GPA/GPB
+    holdingRegisters[i+7] = mcp0.digitalRead(i+8);
+    holdingRegisters[i+14] = mcp1.digitalRead(i);    // Reading MCP1 GPA/GPB
+    holdingRegisters[i+14+7] = mcp1.digitalRead(i+8);
+  }
+
+  int regNext = 28;
+  for (int i=BOARD_IN_FIRST; i<= BOARD_IN_LAST; i++){
+    holdingRegisters[regNext] = digitalRead(i);     // Reading Board pins
+    regNext++;
+  }
+}
--- TestBoard/src/main.cpp
+++ TestBoard/src/main.cpp
@@ -1,102 +1,73 @@
 #include <Arduino.h>
 #include <SimpleModbusSlave.h>
+#include <MCP23017.h>
 
-#define BITS 11  ///< Encoder data bits length
-#define PRINT_SPEED 50 // Per second
+#define PRINT_SPEED 100 // Modbus update per second
 #define BAUD_RATE 9600
+#define BITS 64
+#define MCP_LAST_PIN 6 // MCP23017 has problematic GPA7/GPB7
+#define BOARD_IN_FIRST 7
+#define BOARD_IN_LAST 10
+
+MCP23017 mcp0 = MCP23017(0x20);
 
 void gpioInit();
-void readRaw(int *a);
-void printRaw(int *a);
-int bcd8421(int *a, int length);
-int rawToBin(int *a);
+void mcpInit();
+void registersInit();
+void readPins();
 
-const int MODBUS_ID = 3;
-const int HOLDING_REGS_SIZE = 1;
-unsigned int modbusHoldingRegisters[HOLDING_REGS_SIZE] = {0};
 
-const byte PINS [BITS] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+const int MODBUS_ID = 10;
+const int HOLDING_REGS_SIZE = 32;
+unsigned int holdingRegisters[HOLDING_REGS_SIZE] = {};
+
 int RAWVALUES [BITS] = {};
 
 void setup() {
   // Serial.begin(BAUD_RATE);
+  registersInit();
   gpioInit();
+  mcpInit();
   modbus_configure(BAUD_RATE, MODBUS_ID, 1, HOLDING_REGS_SIZE, 0);
 }
 
 void loop() {
-  readRaw(RAWVALUES);
-  int degree = rawToBin(RAWVALUES);
-  modbusHoldingRegisters[0] = degree;
-  // printRaw(RAWVALUES); Serial.print("\t");
-  // Serial.println( modbusHoldingRegisters[0] );
-  modbus_update(modbusHoldingRegisters);
+  readPins();
+  modbus_update(holdingRegisters);
   delay(PRINT_SPEED);
 }
 
+void registersInit() {
+  for (int i=0; i< HOLDING_REGS_SIZE; i++) holdingRegisters[i] = 0;
+}
+
 void gpioInit() {
-  for (int i = 0; i < BITS; i++) {
-    pinMode(PINS[i], INPUT_PULLUP);
+  for (int i=BOARD_IN_FIRST; i<= BOARD_IN_LAST; i++) pinMode(i, INPUT_PULLUP);
+}
+
+/**
+ * \brief Inits MCP23017 devices.
+ */
+void mcpInit() {
+  // Init MCP23017 adressing pins
+  pinMode(A1, OUTPUT); digitalWrite(A1, LOW);  // MCP0, addr 20
+  pinMode(A2, OUTPUT); digitalWrite(A2, LOW);
+  pinMode(A3, OUTPUT); digitalWrite(A3, LOW);
+  mcp0.init();
+  for (int i=0; i<= MCP_LAST_PIN; i++) { // GPA/GPB init
+     mcp0.pinMode(i, INPUT_PULLUP); mcp0.pinMode(i+8, INPUT_PULLUP);
   }
 }
 
-/**
- * \brief Converts BCD 8421 code to a decimal value.
- *
- * \param[*a]      the pointer to the binary data array. Data order: 1 2 4 8.
- * \param[length]  data length, max value is 4.
- * \returns        0-9 value.
- */
-int bcd8421(int *a, int length) {
-  const int weight[] = {1, 2, 4, 8};
-  int sum = 0;
-  for (int i=0; i<length; ++i)
-    sum += a[i]*weight[i];
-  return sum;
-};
-
-/**
- * \brief Reads encoder binarry data to \param[*a]
- */
-void readRaw(int *a) {
-  int readVal;
-  for (int i = 0; i < BITS; i++) {
-    readVal = !digitalRead(PINS[i]);
-    a[i] = readVal;
+void readPins() {
+  for (int i=0; i<= MCP_LAST_PIN; i++) {
+    holdingRegisters[i] = mcp0.digitalRead(i);       // Reading MCP0 GPA/GPB
+    holdingRegisters[i+7] = mcp0.digitalRead(i+8);
   }
-}
 
-/**
- * \brief Pretty print \param[*a] binary data without ending \n
- */
-void printRaw(int *a) {
-  for(int j = BITS-1; j >=0; j--) {
-    if (j==7 || j == 3) {
-      Serial.print(" ");
-    };
-    Serial.print(a[j]);
-  }
-}
-
-
-/**
- * \brief Converts raw alue to decimal value.
- *
- * \param[*a]    the pointer to the binary data array.
- * \returns      value in the range of 0-719 .
- */
-int rawToBin(int *a) {
-  int x1   = bcd8421(a, 4);
-  int x10  = bcd8421(&a[4], 4) * 10;
-  int x100 = bcd8421(&a[8], 3) * 100;
-  return x1+x10+x100;
-}
-
-/**
- * \brief Grey code test implementation.
- */
-void greytobinary(int *a, int count) {
-  for(int i = count - 2; i >= 0; i--) {
-    a[i] = a[i]^a[i+1];
+  int regNext = 28;
+  for (int i=BOARD_IN_FIRST; i<= BOARD_IN_LAST; i++){
+    holdingRegisters[regNext] = digitalRead(i);     // Reading Board pins
+    regNext++;
   }
 }
