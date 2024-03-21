29b110074c070575414630568ea0f7ac7f65c847
MichaelKazarian <28EMACSlisp>
2024-03-21 09:26:20 +0200

    Description was added

--- TestBoard/src/main.cpp
+++ TestBoard/src/main.cpp
@@ -1,3 +1,25 @@
+/**
+ * Sample of use.
+ * mbpoll -m rtu -a 10 -b 9600 -t 4 -r 1 -P none /dev/ttyUSB0 1 0 1
+ * It works with an external 485 to TTL adapter.
+ * Arduino nano internal USB adapter crashes with Connection timeout.
+ * Also 2-3 connection timeout caught while mbpoll starts reading:
+ * mbpoll -m rtu -a 10 -b 9600 -t 4 -r 1 -P none /dev/ttyUSB0
+ * ....
+ * Data type.............: 16-bit register, output (holding) register table
+ *
+ * -- Polling slave 10... Ctrl-C to stop)
+ * Read output (holding) register failed: Connection timed out
+ * -- Polling slave 10... Ctrl-C to stop)
+ * [1]:    0
+ * -- Polling slave 10... Ctrl-C to stop)
+ * [1]:    0
+ * ^C--- /dev/ttyUSB0 poll statistics ---
+ * 3 frames transmitted, 2 received, 1 errors, 33.3% frame loss
+ * TODO: Possible DTR interference, try to run using USB to TTL
+ * adapter with RX/TX only
+ */
+
 #include <Arduino.h>
 #include <SimpleModbusSlave.h>
 #include <MCP23017.h>
@@ -14,7 +36,7 @@ MCP23017 mcp0 = MCP23017(0x20);
 void gpioInit();
 void mcpInit();
 void registersInit();
-void readPins();
+void writePins();
 
 
 const int MODBUS_ID = 10;
@@ -24,17 +46,18 @@ unsigned int holdingRegisters[HOLDING_REGS_SIZE] = {};
 int RAWVALUES [BITS] = {};
 
 void setup() {
-  // Serial.begin(BAUD_RATE);
+  while (!Serial) { delay(10); }
+  Serial.begin(BAUD_RATE);
   registersInit();
   gpioInit();
   mcpInit();
-  modbus_configure(BAUD_RATE, MODBUS_ID, 1, HOLDING_REGS_SIZE, 0);
+  modbus_configure(BAUD_RATE, MODBUS_ID, 13, HOLDING_REGS_SIZE, 0);
 }
 
 void loop() {
-  readPins();
   modbus_update(holdingRegisters);
-  delay(PRINT_SPEED);
+  writePins();
+  // delay(PRINT_SPEED);
 }
 
 void registersInit() {
@@ -42,7 +65,7 @@ void registersInit() {
 }
 
 void gpioInit() {
-  for (int i=BOARD_IN_FIRST; i<= BOARD_IN_LAST; i++) pinMode(i, INPUT_PULLUP);
+  for (int i=BOARD_IN_FIRST; i<= BOARD_IN_LAST; i++) pinMode(i, OUTPUT);
 }
 
 /**
@@ -55,19 +78,12 @@ void mcpInit() {
   pinMode(A3, OUTPUT); digitalWrite(A3, LOW);
   mcp0.init();
   for (int i=0; i<= MCP_LAST_PIN; i++) { // GPA/GPB init
-     mcp0.pinMode(i, INPUT_PULLUP); mcp0.pinMode(i+8, INPUT_PULLUP);
+     mcp0.pinMode(i, OUTPUT); mcp0.pinMode(i+8, OUTPUT);
   }
 }
 
-void readPins() {
-  for (int i=0; i<= MCP_LAST_PIN; i++) {
-    holdingRegisters[i] = mcp0.digitalRead(i);       // Reading MCP0 GPA/GPB
-    holdingRegisters[i+7] = mcp0.digitalRead(i+8);
-  }
-
-  int regNext = 28;
-  for (int i=BOARD_IN_FIRST; i<= BOARD_IN_LAST; i++){
-    holdingRegisters[regNext] = digitalRead(i);     // Reading Board pins
-    regNext++;
-  }
+void writePins() {
+  mcp0.digitalWrite(4, holdingRegisters[0]);
+  mcp0.digitalWrite(5, holdingRegisters[1]);
+  mcp0.digitalWrite(6, holdingRegisters[2]);
 }
