42c2ceb7f1cb5c49b0e17749830cde690e55849d
MichaelKazarian <28EMACSlisp>
2024-05-01 17:34:57 +0300

    Read test board sensors

---
+++ WEBAPP/routes/communicator.js
@@ -0,0 +1,93 @@
+const ModbusRTU = require("modbus-serial");
+
+class Mode {
+    
+}
+
+class ModeManual extends Mode {
+    
+}
+
+class ModeAuto extends Mode {
+    
+}
+
+class Communicator {
+#inputState;
+#mode;
+#device
+    constructor(modbusId=20) {
+        this.connectionInit(modbusId);
+        this.#mode = new ModeManual();
+    }
+
+    /**
+     * Open connection to a serial port
+     */
+    connectionInit(modbusId) {
+        this.#device = new ModbusRTU();
+        this.#device.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 9600 });
+        this.#device.setTimeout(500);
+        this.#device.setID(modbusId);
+        this.#inputState = {
+            degree: -1,
+            input: "[]",
+            error: ""
+        };
+    }
+
+    get inputState() {
+        return this.#inputState;
+    }
+
+    /**
+     * Read the 3 registers starting at address 0 on device number this.#device
+     * It is similar to executing
+     * mbpoll -0 -m rtu -a 20 -b 9600 -t 4 -r 0 -c 3 -P none /dev/ttyUSB0
+     */
+    read = () => {
+        this.#device.readHoldingRegisters(0, 3, (err, data) => {
+                this.#inputState.input = (data.data);
+            });
+    }
+
+    /**
+     * write the values [0 ... 0xffff] to registers starting at address 3
+     * It is similar to executing
+     * mbpoll -0 -m rtu -a 20 -b 19200 -t 4 -r 3 -P none /dev/ttyUSB0 1 0 1
+     */
+    write = (data) => {
+        this.#device.writeRegisters(3, data);
+        then(console.log);
+    }
+}
+
+let communicator = new Communicator();
+// let manualState = false;
+
+let allowInterval = true;
+process.on('message', (msg) => {
+    console.log('Message from parent:', msg, typeof(msg));
+    if (msg === "STOP") {
+        allowInterval = false;
+    }
+    if (msg === "read") {
+        communicator.read();
+    }
+});
+
+var i = setInterval(() => {
+    if (!allowInterval) {
+        clearInterval(i);
+        process.exit();
+    }
+    communicator.read();
+    // console.log("XZ", communicator.inputState);
+    process.send(JSON.stringify(communicator.inputState));
+}, 100);
+
+// process.on('SIGINT', () => {
+//     console.log(
+//         `Child process terminated due to receipt of SIGINT`);
+//     process.exit(0);
+// });
--- WEBAPP/routes/index.js
+++ WEBAPP/routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const { fork } = require('child_process');
 
+const communicator = fork('routes/communicator.js');
 const router = express.Router();
+
+communicator.on("close", (msg) => {
+    console.log('Child exited', msg);
+});
 
 // use res.render to load up an ejs view file
 router.get('/', function(req, res) {  // index page
@@ -9,6 +15,16 @@ router.get('/', function(req, res) {  // index page
 
 router.get('/test', (req, res) => {
     res.send('It works!');
+});
+
+let x = "";
+router.get('/state', (req, res) => {
+    res.send(JSON.parse(x));
+});
+
+communicator.on('message', msg => {
+    console.log("CHILD MSG", msg);
+    x = msg;//JSON.parse(msg);
 });
 
 module.exports = router;
